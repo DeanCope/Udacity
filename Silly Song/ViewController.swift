f7c7d51471edf00e3404752246202639a5429e6b
DeanCope <deancope@aol.com>
2017-03-09 16:52:04 -0500

    Initial

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
 
+    @IBAction func displayLyrics(_ sender: Any) {
+        if let name = nameField.text {
+            lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: name)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +36,43 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+func shortNameFromName(name: String) -> String {
+    
+    let name = name.lowercased()
+    var shortName = name
+    // find the first vowel
+    if let index = name.rangeOfCharacter(from: CharacterSet(charactersIn: "aeiou")) {
+        // short name starts with the first vowel
+        shortName = name.substring(from: index.lowerBound)
+    }
+    
+    return shortName
+    
+}
+
+// join an array of strings into a single template string:
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameFromName(name: fullName)
+    
+    var returnString = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    returnString = returnString.replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    return returnString
 }
 
